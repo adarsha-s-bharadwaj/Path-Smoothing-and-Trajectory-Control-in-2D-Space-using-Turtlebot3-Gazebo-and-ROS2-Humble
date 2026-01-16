254cb3f357be81b9c57c2e8d56742af2035c286d
Adarsha S <77356989+adarsha-s-bharadwaj@users.noreply.github.com>
2026-01-17 00:13:52 +0530

    Update trajectory_generator.py

--- nav_assignment/trajectory_generator.py
+++ nav_assignment/trajectory_generator.py
@@ -1,3 +1,18 @@
+"""
+trajectory_generator.py
+
+This module is responsible for generating a smooth, time-parameterized
+trajectory from discrete waypoints stored in a YAML file.
+
+Key features:
+- Loads waypoints from ROS 2 package config
+- Uses cubic B-spline interpolation for geometric smoothing
+- Assigns timestamps assuming constant linear velocity
+
+Output:
+- A list of (x, y, t) tuples where t is time from trajectory start
+"""
+
 import yaml
 import os
 import numpy as np
@@ -5,6 +20,24 @@ from ament_index_python.packages import get_package_share_directory
 from scipy.interpolate import splprep, splev
 
 def generate_trajectory(constant_velocity=0.25, ds=0.05):
+    """
+    Generate a smooth, time-parameterized trajectory.
+
+    Parameters
+    ----------
+    constant_velocity : float
+        Desired linear velocity of the robot (m/s).
+    ds : float
+        Approximate spatial resolution used to sample the spline.
+
+    Returns
+    -------
+    trajectory : list of tuples
+        List of (x, y, t) where:
+        - x, y : position in meters
+        - t    : time from trajectory start in seconds
+    """
+    
     pkg_path = get_package_share_directory('nav_assignment')
     yaml_path = os.path.join(pkg_path, 'config', 'waypoints.yaml')
 
@@ -18,13 +51,19 @@ def generate_trajectory(constant_velocity=0.25, ds=0.05):
     tck, _ = splprep([x, y], s=0.0)
     u = np.linspace(0, 1, int(len(x) / ds * 5))
     xs, ys = splev(u, tck)
-
+    
+    # ----------------------------------------------------
+    # Time parameterization (constant velocity assumption)
+    # ----------------------------------------------------
     trajectory = []
     time = 0.0
 
     for i in range(len(xs)):
         if i > 0:
+            # Euclidean distance between consecutive points
             dist = np.hypot(xs[i] - xs[i-1], ys[i] - ys[i-1])
+            
+            # Time increment = distance / velocity
             time += dist / constant_velocity
 
         trajectory.append((xs[i], ys[i], time))
